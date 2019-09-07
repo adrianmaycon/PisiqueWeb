79608c466ade45945917790ddf823d26ca725320
adrianmaycon <mayconlemos1010@gmail.com>
2019-09-07 11:32:42 -0300

    asd

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,71 +1,14 @@
 import React from "react";
-import app from "../base";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
 
-import Dashboard from '../Components/Dashboard';
-import './Styles/index.css';
+import Dashboard from '../Components/Dashboard/index.jsx';
+import NavBar from '../Components/NavBar/index.jsx';
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <div className="List-Body">
-        <Navbar className="Nab-Bar" color="light" light expand="md">
-          <NavbarBrand href="/"><h6><font size="6" face="maiandra gd">Grupo Pisiquê</font></h6></NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#">Biblioteca</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Instituições</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Configurações
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Meu Perfil
-                  </DropdownItem>
-                  <DropdownItem>
-                    Histórico
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem onClick={() => app.auth().signOut()}>
-                    Sair da Conta
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-
+        <NavBar />
         <Dashboard />
       </div>
     );
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Screens/Home";
-import Login from "./Screens/Login";
-import SignUp from "./Screens/SignUp";
-import { AuthProvider } from "./Auth";
-import PrivateRoute from "./PrivateRoute";
-import './Styles/index.css'
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import Home from "../Screens/Home";
+import Login from "../Screens/Login";
+import SignUp from "../Screens/SignUp";
+import { AuthProvider } from "../Auth";
+import PrivateRoute from "../PrivateRoute";
+import './index.css'
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
             <PrivateRoute exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
+            <Redirect from='*' to='/' />
           </div>
         </Router>
       </AuthProvider>
