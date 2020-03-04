4cf9ca9cc6409528d40084b1573cc7dc0cb2aa72
adrianmaycon <mayconlemos1010@gmail.com>
2020-03-04 17:28:01 -0300

    Atualização

--- src/Views/Login/Components/Login.jsx
+++ src/Views/Login/Components/Login.jsx
@@ -2,10 +2,7 @@ import React, { useCallback, useContext } from 'react';
 import { withRouter, Redirect, Link } from "react-router-dom";
 import app from "../../../base.js";
 import { AuthContext } from "../../../Auth.js";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import { Typography, Grid, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import './Css/styles.css';
 
@@ -36,16 +33,17 @@ const Login = ({ history }) => {
   }
 
   return (
-    <body>
-      <div>
-        <div class={`container ${active ? 'right-panel-active' : ''}`} id="container">
+    <Grid className={classes.body}>
+      {/* <div class="central"> */}
+      <Grid className={classes.central}>
+        <div style={{ width: 700 }} class={`container ${active ? 'right-panel-active' : ''}`} id="container">
           <div class="form-container sign-up-container">
             <form action="#">
               <h1>Criar Conta</h1>
               <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                {/* <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
                 <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> */}
               </div>
               <span>ou use seu email para registro</span>
               <input type="text" placeholder="Nome" />
@@ -55,7 +53,48 @@ const Login = ({ history }) => {
             </form>
           </div>
           <div class="form-container sign-in-container">
-            <form noValidate onSubmit={handleLogin}>
+            <form className={classes.form} noValidate onSubmit={handleLogin}>
+              <h1>Login</h1>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                placeholder="Digite seu email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                variant="contained" 
+                color="primary" 
+                disableElevation
+                // fullWidth
+                className={classes.submit}
+              >
+                Entrar
+              </Button>
+              <Grid style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Link onClick={() => setActive(true)}>
+                  {" Não possui uma conta? Inscrever-se"}
+                </Link>
+              </Grid>
+            </form>
+            {/* <form noValidate onSubmit={handleLogin}>
               <h1>Login</h1>
               <div class="social-container">
                 <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
@@ -65,9 +104,9 @@ const Login = ({ history }) => {
               <span>ou use sua conta</span>
               <input type="email" placeholder="Email" />
               <input style={{ marginTop: 5 }} type="password" placeholder="Senha" />
-              <a href="#">Esqueceu sua senha?</a>
+              <Typography variant="caption" display="block" gutterBottom style={{ marginTop: 10 }}> Esqueceu sua senha?</Typography>
               <button style={{ borderRadius: 30 }}>Entrar</button>
-            </form>
+            </form> */}
           </div>
           <div class="overlay-container">
             <div class="overlay">
@@ -84,57 +123,28 @@ const Login = ({ history }) => {
             </div>
           </div>
         </div>
-      </div>
-    </body>
-
-    // <div className={classes.paper}>
-    //   <Typography component="h1" variant="h5">
-    //     Login
-    //   </Typography>
-    //   <form className={classes.form} noValidate onSubmit={handleLogin}>
-    //     <TextField
-    //       variant="outlined"
-    //       margin="normal"
-    //       required
-    //       fullWidth
-    //       id="email"
-    //       label="Email"
-    //       placeholder="Digite seu email"
-    //       name="email"
-    //       autoComplete="email"
-    //       autoFocus
-    //     />
-    //     <TextField
-    //       variant="outlined"
-    //       margin="normal"
-    //       required
-    //       fullWidth
-    //       name="password"
-    //       label="Senha"
-    //       type="password"
-    //       id="password"
-    //       autoComplete="current-password"
-    //     />
-    //     <Button
-    //       type="submit"
-    //       fullWidth
-    //       variant="contained"
-    //       color="primary"
-    //       className={classes.submit}
-    //     >
-    //       Entrar
-    //     </Button>
-    //     <Grid style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-    //       <Link to="/register">
-    //         {" Não possui uma conta? Inscrever-se"}
-    //       </Link>
-    //     </Grid>
-    //   </form>
-    // </div>
+      {/* </div> */}
+      </Grid>
+    </Grid>
   );
 }
 
 const useStyles = makeStyles(theme => ({
+  body: {
+    backgroundImage: 'url(https://conteudo.imguol.com.br/c/noticias/26/2019/12/20/tecnologia-dados-saude-health-tech-digital-digitalizacao-1576876624906_v2_1920x1280.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+  },
+  central: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    fontFamily: 'Montserrat, sans-serif',
+    height: '98.3vh',
+  },
   paper: {
     position: 'relative',
     margin: theme.spacing(8, 4),
@@ -148,7 +158,19 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    borderRadius: '20px',
+    border: '1px solid #5c7bd7',
+    backgroundColor: '#5c7bd7',
+    color: '#FFFFFF',
+    fontSize: '12px',
+    marginTop: 15,
+    width: 200,
+    textTransform: 'none',
+    fontWeight: 'bold',
+    padding: '12px 45px',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    transition: 'transform 80ms ease-in',
   },
 }));
 
