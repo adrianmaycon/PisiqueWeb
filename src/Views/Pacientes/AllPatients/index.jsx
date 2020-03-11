63386ad6d94ef3a8123a246d41ac7f6aa5f98284
Adrian Maycon <mayconlemos1010@gmail.com>
2020-03-11 16:04:51 -0300

    Atualização

--- src/Views/Pacientes/AllPatients/index.jsx
+++ src/Views/Pacientes/AllPatients/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+
+
+
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography, Paper } from '@material-ui/core';
-import TablePatients from './tablePatients.jsx';
+import { Link } from "react-router-dom";
+import { Paper, Tooltip, TablePagination, IconButton, Grid, Typography } from '@material-ui/core';
+import PatientsService from '../../../Services/PatientService';
+
 export default function List() {
     const classes = useStyles();
+    const [patients, setPatients] = useState([]);
+
+    console.log(patients)
+
+    useEffect(() => {
+        PatientsService.getPatients()
+            .then((patient) => { setPatients(patient) })
+    }, []);
 
     return (
         <Grid className={classes.container}>
@@ -13,7 +28,63 @@ export default function List() {
                 <Grid style={{ width: '96%', height: '100%', maxWidth: 1700, minWidth: 850, minHeight: 1000 }}>
                     <Paper className={classes.paper}>
                         <Typography variant='h5'>Pacientes</Typography>
-                        <TablePatients />
+                        <MaterialTable
+                            title="Listagem dos Pacientes"
+                            columns={[
+                                { title: 'CPF', field: 'cpf' },
+                                { title: 'Nome', field: 'name' },
+                                { title: 'Telefone', field: 'tel01' },
+                                { title: 'Data de Nascimento', field: 'dataNasc' },
+                            ]}
+                            localization={{
+                                pagination: {
+                                    labelDisplayedRows: '{from}-{to} de {count}',
+                                    firstTooltip: 'Primeira página',
+                                    previousTooltip: 'Página anterior',
+                                    nextTooltip: 'Próxima página',
+                                    lastTooltip: 'Última página',
+                                    labelRowsSelect: 'Linhas'
+                                },
+                                toolbar: {
+                                    searchTooltip: 'Buscar',
+                                    searchPlaceholder: 'Buscar',
+                                    nRowsSelected: '{0} Documento(s) selecionado'
+                                },
+                                header: {
+                                    actions: 'Ações',
+                                },
+                                body: {
+                                    emptyDataSourceMessage: 'Não há registros a serem exibidos',
+                                }
+                            }}
+                            data={patients}
+                            actions={[
+                                {
+                                    icon: 'assignment',
+                                    tooltip: 'Visualizar Parciente',
+                                    onClick: (event, rowData) => null
+                                }
+                            ]}
+                            options={{ actionsColumnIndex: -1, draggable: false, sorting: false, pageSize: 10 }}
+                            components={{
+                                Container: props => <Paper {...props} elevation={0} />,
+                                Pagination: props => (
+                                    <TablePagination
+                                        {...props}
+                                        rowsPerPageOptions={[10, 15, 20]}
+                                    />
+                                ),
+                                Action: props =>
+                                    <Tooltip title="Visualizar Parciente">
+                                        <Link to={`/details/${props.data.id}`}>
+                                            <IconButton>
+                                                <AssignmentIcon />
+                                            </IconButton>
+                                        </Link>
+                                    </Tooltip>
+                            }}
+                            onRowClick={(event, rowData, togglePanel) => null}
+                        />
                     </Paper>
                 </Grid>
             </Grid>
