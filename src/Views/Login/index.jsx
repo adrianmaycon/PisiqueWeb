11839c123c24409722db70b8e88e968ea12256a3
adrianmaycon <mayconlemos1010@gmail.com>
2020-03-15 21:52:11 -0300

    att

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -1,17 +1,19 @@
 import React, { useCallback, useContext } from 'react';
-import { withRouter, Redirect, Link } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
 import app from "../../base.js";
 import { AuthContext } from "../../Auth.js";
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import ReportProblemRoundedIcon from '@material-ui/icons/ReportProblemRounded';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import { IconButton, Grid, TextField } from '@material-ui/core';
 import './Css/styles.css';
 
-import { Container, Body, Form, Button, ButtonSecundary, Title } from './styled'
+import { Container, Body, Form, Button, ButtonSecundary, Title, Link, Info } from './styled'
 
 const Login = ({ history }) => {
   const [active, setActive] = React.useState(false);
+  const [info, setInfo] = React.useState(false);
 
   const handleLogin = useCallback(
     async event => {
@@ -23,7 +25,8 @@ const Login = ({ history }) => {
           .signInWithEmailAndPassword(email.value, password.value);
         history.push("/");
       } catch (error) {
-        alert('Login ou Senha inválidos!');
+        setInfo(true)
+        setTimeout(() => { setInfo(false) }, 3000);
       }
     },
     [history]
@@ -42,8 +45,12 @@ const Login = ({ history }) => {
   return (
     <Container>
       <Body>
-        <div style={{ width: '700px' }} class={`container ${active ? 'right-panel-active' : ''}`} >
-          <div class="form-container sign-up-container">
+        {info ? <Info>
+          <ReportProblemRoundedIcon style={{ color: '#ffa117', fontSize: '35px' }} />
+          <h5>Atenção! Login ou Senha inválidos!</h5>
+        </Info> : ''}
+        <div style={{ width: '700px' }} className={`container ${active ? 'right-panel-active' : ''}`} >
+          <div className="form-container sign-up-container">
             <Form>
               <Title size={40}>Criar Conta</Title>
               <TextField
@@ -87,14 +94,14 @@ const Login = ({ history }) => {
               />
               <Button onClick={handleRegister}>Cadastrar</Button>
               <Grid style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Link href="/" onClick={() => setActive(false)}>
+                <Link onClick={() => setActive(false)}>
                   Já possui uma conta? Fazer login
                 </Link>
               </Grid>
             </Form>
           </div>
 
-          <div class="form-container sign-in-container">
+          <div className="form-container sign-in-container">
             <Form noValidate onSubmit={handleLogin}>
               <Title size={40}>Login</Title>
               <Grid style={{ display: 'flex', width: '70%', justifyContent: 'space-around', marginBottom: 20, marginTop: 20 }}>
@@ -114,7 +121,7 @@ const Login = ({ history }) => {
                 margin="normal"
                 required
                 fullWidth
-                id="email"
+                id="emailLogin"
                 label="Email"
                 placeholder="Digite seu email"
                 name="email"
@@ -132,25 +139,25 @@ const Login = ({ history }) => {
                 name="password"
                 label="Senha"
                 type="password"
-                id="password"
+                id="passwordLogin"
                 autoComplete="current-password"
               />
               <Button type="submit" >Entrar</Button>
               <Grid style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Link href="/" onClick={() => setActive(true)}>
+                <Link onClick={() => setActive(true)}>
                   Não possui uma conta? Inscrever-se
                 </Link>
               </Grid>
             </Form>
           </div>
-          <div class="overlay-container">
-            <div class="overlay">
-              <div class="overlay-panel overlay-left">
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
                 <Title size={50}>Pisiquê</Title>
                 <p style={{ marginTop: 25, marginBottom: 25 }}>Para se manter conectado, faça login com suas informações pessoais</p>
                 <ButtonSecundary onClick={() => setActive(false)}>Entrar na Conta</ButtonSecundary>
               </div>
-              <div class="overlay-panel overlay-right">
+              <div className="overlay-panel overlay-right">
                 <Title size={50}>Pisiquê</Title>
                 <p style={{ marginTop: 25, marginBottom: 25 }}>Digite seus dados pessoais e comece a jornada conosco</p>
                 <ButtonSecundary onClick={() => setActive(true)}>Inscrever-se</ButtonSecundary>
