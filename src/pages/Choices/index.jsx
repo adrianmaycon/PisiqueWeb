d9c8283d32531dfe7d2ccd2a471265cd69316868
adrianmaycon <mayconlemos1010@gmail.com>
2020-09-05 17:11:37 -0300

    atualização

--- src/pages/Choices/index.jsx
+++ src/pages/Choices/index.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import psi from '../../assets/images/icons/psi.svg';
+import study from '../../assets/images/icons/estudante.svg';
+import userGp from '../../assets/images/icons/userGp.svg';
 
 import './styles.css';
+
+import { FaArrowRight } from "react-icons/fa";
 
 function Choices() {
     const [choice, setChoice] = useState(2);
@@ -12,21 +17,31 @@ function Choices() {
             </header>
 
             <main>
-                <section onClick={() => setChoice(1)} className={choice === 1 && "select" }>
+                <section onClick={() => setChoice(1)} className={choice === 1 && "select"}>
                     <h6>Psicólogo</h6>
+                    <img src={psi} alt="Psicólogo homem" />
+                    <h6>{choice === 1 && 'Selecionado'}</h6>
                 </section>
 
-                <section onClick={() => setChoice(2)} className={choice === 2 && "select" }>
+                <section onClick={() => setChoice(2)} className={choice === 2 && "select"}>
                     <h6>Usuário GP</h6>
+                    <img src={userGp} alt="Psicólogo homem" />
+                    <h6>{choice === 2 && 'Selecionado'}</h6>
                 </section>
 
-                <section onClick={() => setChoice(3)} className={choice === 3 && "select" }>
+                <section onClick={() => setChoice(3)} className={choice === 3 && "select"}>
                     <h6>Estudante</h6>
+                    <img src={study} alt="Psicólogo homem" />
+                    <h6>{choice === 3 && 'Selecionado'}</h6>
                 </section>
             </main>
 
             <footer>
-                <h1>Continuar como {choice === 1 ? "Psicólogo" : choice === 2 ? "Usuário GP" : "Estudante"}</h1>
+                <p>{choice === 1 ? 'Conteudos exclusivos, realizar atendimento, tudo que é necessário para você em uma plataforma' : choice === 2 ? 'Acesso a todos os profissionais e conteúdos cadastrados na plataforma' : 'Acesso a conteúdos exclusivos, interação com outros estudantes de psicologia e muito mais'}</p>
+                <div className="row">
+                    <h1>Continuar como {choice === 1 ? "Psicólogo" : choice === 2 ? "Usuário GP" : "Estudante"}</h1>
+                    <button type="button"><FaArrowRight /></button>
+                </div>
             </footer>
         </div>
     )
