9cdb0ece6b69579bca71b52fb6c6642a5f62fad6
adrianmaycon <mayconlemos1010@gmail.com>
2020-09-21 00:22:44 -0300

    -Adição imagem dashboard

--- src/pages/Dashboard/Painel/index.jsx
+++ src/pages/Dashboard/Painel/index.jsx
@@ -1,27 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 
+import UsersService from '../../../services/UsersService';
+import { AuthContext } from '../../../auth/AuthContext';
+import { authConfig } from '../../../auth/config';
 import './styles.css';
 
 function Painel() {
+
     const [openConfig, setOpenConfig] = useState(false)
+    const [dataUser, setDataUser] = useState({})
+
+    const { usuario } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (!!usuario) {
+            UsersService.GetDataUser(usuario.email)
+                .then(user => {
+                    console.log(user)
+                    setDataUser(user)
+                })
+        }
+
+    }, [usuario])
+
+    const DivPopover = () => (
+        <div className="div-popover">
+            <main>
+                <a href="/dashboard">Painel</a>
+                <a href="/dashboard">Histórico</a>
+                <a href="/dashboard">Meus Livros</a>
+                <a href="/" onClick={() => authConfig.auth().signOut()}>Sair</a>
+            </main>
+        </div>
+    )
 
     return (
         <div className="container-painel">
             <section id="sectionPrimary">
                 <header>
-                    <h1 id="name">Olá, Adrian Maycon</h1>
+                    <h1 id="name">Olá {String(dataUser.name).split(" ", 1).join(' ')}, seja bem vindo! </h1>
 
-                    <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => setOpenConfig(!openConfig)}>
-                        <img src={'https://vercel.com/api/www/avatar/b943b7f78e7e04c50c3c2d57bf26cc4d5829dcd0?s=60'} alt="Abrir configuração de perfil" />
-                        {openConfig ? <FaCaretUp /> : <FaCaretDown />}
+                    <div>
+                        <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => setOpenConfig(!openConfig)}>
+                            <img src={dataUser.avatar} alt="Abrir configuração de perfil" />
+                            {openConfig ? <FaCaretUp /> : <FaCaretDown />}
+                        </div>
+                        {openConfig && <DivPopover />}
                     </div>
                 </header>
             </section>
 
             <section id="sectionSecundary">
+                <header>
+                    <img src={dataUser.avatar} alt="Foto de perfil do usuario" />
 
+                    <h1>{String(dataUser.name).split(" ", 2).join(' ')}</h1>
+                    <h4>{dataUser.email}</h4>
+                </header>
             </section>
         </div>
     )
