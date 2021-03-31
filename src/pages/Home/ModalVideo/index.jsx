f70ef4361118accd1518cf1d052bb0d07f2f62ad
adrianmaycon <mayconlemos1010@gmail.com>
2021-03-31 10:21:26 -0300

    Ajuste iframe

--- src/pages/Home/ModalVideo/index.jsx
+++ src/pages/Home/ModalVideo/index.jsx
@@ -9,9 +9,9 @@ function ModalVideo({close}) {
                 className="youtube-video"
                 src="https://www.youtube.com/embed/GwpdmkBUtL8"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
+                allowFullScreen
             />
         </Container>
     )
